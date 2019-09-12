e91829d9e68df1207c507f2201ee9425eec49850
ex-hibition <nozomumumu@gmail.com>
2019-09-12 22:43:57 +0900

    メッセージサービス対応

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -3,15 +3,20 @@ import { Observable, of } from 'rxjs';
 
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
+import { MessageService } from './message.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
 
-  constructor() { }
+  constructor(
+    private messageService: MessageService
+  ) { }
 
   getHeroes(): Observable<Hero[]> {
+    // TODO: send the messages _after_ fetching the heroes
+    this.messageService.add('HeroService: fetched heroes')
     return of(HEROES);
   }
 }
