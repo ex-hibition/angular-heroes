21e124ac259af3b9574de192db1ce935dd944bab
ex-hibition <nozomumumu@gmail.com>
2019-09-12 22:15:26 +0900

    observable対応

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { HeroService } from '../hero.service';
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
@@ -9,16 +9,23 @@ import { HEROES } from '../mock-heroes';
   styleUrls: ['./heroes.component.scss']
 })
 export class HeroesComponent implements OnInit {
-  heroes = HEROES;
+  heroes: Hero[];
 
   selectedHero: Hero;
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
   } 
 
-  constructor() { }
+  constructor(
+    private heroService: HeroService
+  ) { }
+
+  getHeroes(): void {
+    this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes)
+  }
 
   ngOnInit() {
+    this.getHeroes();
   }
 
 }
